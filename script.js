ec765852f8fdfe2c03f1a6ec331f01edc267cdb1
VaniaSantos38 <vaniabernard@live.com>
2026-04-29 14:56:15 -0300

    modificação no js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function generateNumber(){ //essa função é chamada no onclick do botão,depois é só criar a função no java script//
     const min =Math.ceil (document.querySelector('.input-min').value) //definindo a variável min, usando o document.querySelector para selecionar o elemento com a classe .input-min e pegando o valor digitado pelo usuário//
     const max = Math.floor(document.querySelector('.input-max').value) //definindo a variável max, usando o document.querySelector para selecionar o elemento com a classe .input-max e pegando o valor digitado pelo usuário//
-  const result = Math.floor(Math.random() * (max - min + 1)) + min;//5 gerando um número aleatório usando Math.random(), multiplicando pelo intervalo (max - min + 1) para garantir que o número gerado esteja dentro do intervalo definido pelo usuário, e adicionando min para ajustar o resultado para o intervalo correto//
+  
 
   if(min >= max){
     alert("O valor Mínimo tem que ser Menor que o valor Máximo")
